1c01da4b1722668ea2563bae8d8abecc53c75bad
Tom Knight <tom.knight@pushdoctor.io>
2021-12-10 22:08:35 +0000

    Done day 6 part 2 done

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -19,16 +19,16 @@ func main() {
 
 	stringLines := strings.Split(string(data), ",")
 
-	var lanternFish = []int{}
+	var lanternFish = []int8{}
 
 	for _, line := range stringLines {
 		val, err := strconv.Atoi(line)
 		check(err)
-		lanternFish = append(lanternFish, val)
+		lanternFish = append(lanternFish, int8(val))
 	}
 
 	// 80 day loop
-	for i := 0; i < 80; i++ {
+	for i := 0; i < 256; i++ {
 		count := len(lanternFish)
 		for lf := 0; lf < count; lf++ {
 			lanternFish[lf] -= 1

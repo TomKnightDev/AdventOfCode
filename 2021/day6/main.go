937733a56779c7b2eb06d257ea56b76bf628931e
Tom Knight <tom.knight@pushdoctor.io>
2021-12-10 23:15:03 +0000

    Day 6 part 2 done

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type lanternFish struct {
+	age   int
+	count int
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -18,26 +23,63 @@ func main() {
 	check(err)
 
 	stringLines := strings.Split(string(data), ",")
-
-	var lanternFish = []int8{}
+	var fish = [9]lanternFish{}
 
 	for _, line := range stringLines {
 		val, err := strconv.Atoi(line)
 		check(err)
-		lanternFish = append(lanternFish, int8(val))
+		fish[val].count++
 	}
 
 	// 80 day loop
 	for i := 0; i < 256; i++ {
-		count := len(lanternFish)
-		for lf := 0; lf < count; lf++ {
-			lanternFish[lf] -= 1
-			if lanternFish[lf] < 0 {
-				lanternFish = append(lanternFish, 8)
-				lanternFish[lf] = 6
-			}
-		}
+		zeroCount := fish[0].count
+
+		fish[0].count = fish[1].count
+		fish[1].count = fish[2].count
+		fish[2].count = fish[3].count
+		fish[3].count = fish[4].count
+		fish[4].count = fish[5].count
+		fish[5].count = fish[6].count
+		fish[6].count = fish[7].count + zeroCount
+		fish[7].count = fish[8].count
+		fish[8].count = zeroCount
+		// fmt.Println(fish)
 	}
 
-	fmt.Print(len(lanternFish))
+	total := 0
+	for _, lf := range fish {
+		total += lf.count
+	}
+
+	fmt.Print(total)
 }
+
+// func main() {
+// 	data, err := os.ReadFile("input.txt")
+// 	check(err)
+
+// 	stringLines := strings.Split(string(data), ",")
+
+// 	var lanternFish = []int8{}
+
+// 	for _, line := range stringLines {
+// 		val, err := strconv.Atoi(line)
+// 		check(err)
+// 		lanternFish = append(lanternFish, int8(val))
+// 	}
+
+// 	// 80 day loop
+// 	for i := 0; i < 256; i++ {
+// 		count := len(lanternFish)
+// 		for lf := 0; lf < count; lf++ {
+// 			lanternFish[lf] -= 1
+// 			if lanternFish[lf] < 0 {
+// 				lanternFish = append(lanternFish, 8)
+// 				lanternFish[lf] = 6
+// 			}
+// 		}
+// 	}
+
+// 	fmt.Print(len(lanternFish))
+// }
